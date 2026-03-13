c219fc5c79f1b4ad01a1ee861528beb1bc952469
Arjun <arjunkannur56@gmail.com>
2026-03-13 14:44:05 +0530

    first commit

---
+++ src/main/java/com/pes/researchportal/ResearchportalApplication.java
@@ -0,0 +1,13 @@
+package com.pes.researchportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pes/researchportal/ResearchportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pes.researchportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
